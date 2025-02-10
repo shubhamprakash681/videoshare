5e2ac9b78c47805caa6ef81ee6f5c2caa16d2f3b
Shubham Prakash <shubhamprakash681@gmail.com>
2025-02-10 08:11:35 +0530

    Add development environment configuration and proxy setup in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import { ServerOptions } from "vite";
+
+const getServerProxy: (env: string) => ServerOptions["proxy"] | undefined = (
+  env
+) => {
+  if (env === "development") {
+    return {
+      "/api": {
+        target: "http://127.0.0.1:5000",
+        changeOrigin: true,
+        secure: false,
+        // Use false if you're not using HTTPS in development
+        // rewrite: (path) => path.replace(/^\/api/, ""), // Optional, modify this based on your API routes
+      },
+    };
+  }
+  return undefined;
+};
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,15 +28,7 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://127.0.0.1:5000",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       // Use false if you're not using HTTPS in development
-  //       // rewrite: (path) => path.replace(/^\/api/, ""), // Optional, modify this based on your API routes
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: { ...getServerProxy(process.env.NODE_ENV || "development") },
+  },
 });
